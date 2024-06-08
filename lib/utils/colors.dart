e8d4b8a0a2936637edc3a192e4c77e473bda54ba
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-08 22:27:19 +0530

    colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color get red => Color(0xFFFF2950);
+  static Color get blue => Color(0xFF192E52);
+  static Color get lynch => Color(0xFF607698);
+  static Color get gray => Color(0xFFE9E9F0);
+}
