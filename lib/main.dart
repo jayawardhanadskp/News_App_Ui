0115d1243aecbcc5d030f89d6b30aed8df34abf8
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-08 22:27:09 +0530

    main

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'screens/01.dart';
+import 'screens/02.dart';
+import 'screens/03.dart';
+import 'screens/04.dart';
+import 'screens/05.dart';
+import 'screens/06.dart';
+import 'screens/07.dart';
+import 'screens/08.dart';
+import 'screens/09.dart';
+import 'screens/10.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Search(),
+    );
+  }
+}
+
+// 2 SecondPage
+// 3 SignIn
+// 4 SignUp
+// 5 signInGoogle
+// 6 ForgetPass
+// 7 Verification
+// 8 UpdatePass
+// 9 Terms
+//10 Search
