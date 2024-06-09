ee18d06121601e136758ab62352f80c14ac325f0
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-10 02:26:51 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home: signInGoogle(),
+      home: News(),
     );
   }
 }
